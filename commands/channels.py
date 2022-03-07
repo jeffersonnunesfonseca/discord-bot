7b6978fa8ce26f01778b246acd5cb0b123127d36
jeffersonnunesfonseca <jeffersonnunesfonseca@gmail.com>
2022-03-07 19:50:30 -0300

    interaçao e validacao de contador ao aceitar funcionando

--- commands/channels.py
+++ commands/channels.py
@@ -1,10 +1,11 @@
 import os
-from turtle import color
+import time
 
 import discord
 
 from discord.ext import commands
 from discord_components import Button
+from asyncio.exceptions import TimeoutError
 
 import utils
 
@@ -41,13 +42,13 @@ class Channels(commands.Cog):
     # bot.command => commands.command
     @commands.command(name="fila", help="Cria uma fila. Args: tipo(emu, mob),tamanho(4x4,1x1..), valor")
     async def create_queue(self, ctx,type,size,price):
+        count_click_btn_accept = 0
+        users_interation = []
         if not utils.check_comando_role_permission(ctx):
             await ctx.message.delete()
             await ctx.send(f"{ctx.author.name}, você não tem permissão!")
             return
         else:
-            await ctx.send(f"{ctx.author.name}, VCÊ TEM A PERMISSÃO CARAAA")
-            
             embed = discord.Embed(
                 title="Fila Criada!",
                 description="escolha a opção desejada nos botões abaixo.",
@@ -65,30 +66,60 @@ class Channels(commands.Cog):
             embed.set_footer(
                 text="Feito por " + self.bot.user.name, icon_url=self.bot.user.avatar_url
             )
-
-
+            
             btn_accept =  Button(label = "Entrar na fila [0/2]", custom_id = "btn_accept",style=1,emoji="✅")
             btn_reject =  Button(label = "Sair da fila", custom_id = "btn_reject", style=4, emoji="❎")
 
 
-            await ctx.send(embed=embed, components = [[btn_accept,btn_reject]])
+            msg = await ctx.send(embed=embed, components = [[btn_accept,btn_reject]])
 
-            # interaction = await self.bot.wait_for("button_click", check = lambda i: i.custom_id == "button1")
-            # import ipdb; ipdb.set_trace()
-            # await interaction.send(content = "Button clicked!")
-            # button1 = Button(label = "button1", custom_id = "button1")
-            # button2 = Button(label = "button2", custom_id = "button2")
-            # button3 = Button(label = "button3", custom_id = "button3")
-            # button1disabled = Button(label = "button1", custom_id = "button1", disabled = True)
-            # button2disabled = Button(label = "button2", custom_id = "button2", disabled = True)
-            # button3disabled = Button(label = "button3", custom_id = "button3", disabled = True)
-            # msg = await ctx.send("button", components = [button1, button2, button3])
-            # interaction = await self.bot.wait_for("button_click", check = lambda inter: inter.custom_id == "button1")
-            # await interaction.respond(type = 7, content = msg, components = [button1disabled, button2disabled, button3disabled])
-            # await interaction.send(f"Disabled")
-            # self.bot.buttons = Buttons(self.bot)
-            # res = await self.bot.buttons.send(ctx.message.channel, "here you go", buttons=[Button("myID", "Press me", emoji="😀")])
-            # url_image = "https://picsum.photos/1920/1080"
+            while True:
+                try:
+
+                    interaction_accept_btn = await self.bot.wait_for("button_click", check = lambda inter: inter.custom_id == "btn_accept",timeout=60)
+                    user_id = interaction_accept_btn.user.id
+                    this_user_has_interation = False
+
+                    if users_interation:
+                        for user in users_interation:
+                            if user.id == user_id:
+                                this_user_has_interation = True
+                                break
+
+                    if not this_user_has_interation:
+                        count_click_btn_accept+=1
+                        
+                        btn_accept.set_label(f"Entrar na fila [{count_click_btn_accept}/2]")
+                        if count_click_btn_accept == 2:
+                            btn_accept.set_disabled(True)                        
+
+                        await interaction_accept_btn.respond(type = 7, components = [[btn_accept, btn_reject]])
+                        users_interation.append(interaction_accept_btn.user)
+                    else:
+                        await ctx.send(f"**{ctx.author.name}** você já esta participando dessa aposta.")
+                        await interaction_accept_btn.respond(type = 7)
+
+                except TimeoutError as e:
+                    await msg.delete()
+                    await ctx.send(f"Aposta cancelada por falta de jogadores ... ")
+                    break
+                    
+
+
+    # async def button(ctx):
+    #     button1 = Button(label = "button1", custom_id = "button1")
+    #     button2 = Button(label = "button2", custom_id = "button2")
+    #     button3 = Button(label = "button3", custom_id = "button3")
+
+    #     button1disabled = Button(label = "button1", custom_id = "button1", disabled = True)
+    #     button2disabled = Button(label = "button2", custom_id = "button2", disabled = True)
+    #     button3disabled = Button(label = "button3", custom_id = "button3", disabled = True)
+
+    #     msg = await ctx.send("button", components = [button1, button2, button3])
+    #     interaction = await bot.wait_for("button_click", check = lambda inter: inter.custom_id == "button1")
+    #     await interaction.respond(type = 7, content = msg, components = [button1disabled, button2disabled, button3disabled])
+    #     await interaction.send(f"Disabled")
+
 
 
 
